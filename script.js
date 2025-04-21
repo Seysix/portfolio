fc0e0a75e6f100ebbe7fd95c35426c9d9e20b292
Tymon Budzowski <tymonbudzowski@MacBook-Pro-Tymon.local>
2025-04-22 01:10:56 +0200

    working dark mode

--- script.js
+++ script.js
@@ -40,3 +40,20 @@ emailLink.addEventListener('click', function(e) {
   const email = this.textContent.trim();
   navigator.clipboard.writeText(email)
 });
+
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode')
+  
+}
+
+themeSwitch.addEventListener("click", () => {
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
